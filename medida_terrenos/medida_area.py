67a1ed5c23ae64988b0de23a80c026468dd09947
camillycr <camillycr03@gmail.com>
2022-04-18 15:54:40 -0300

    first commit

---
+++ medida_terrenos/medida_area.py
@@ -0,0 +1,5 @@
+larg = float(input('Largura do terreno: '))
+comp = float(input('Comprimento do terreno: '))
+area = larg * comp
+print('-=-' * 12)
+print('Nesse caso, o terreno possui {}m² ' .format(area))
